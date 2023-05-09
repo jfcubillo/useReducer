9a034298a40e88ae6b9518e65cccc14a93cefd88
Feli <juan.cubillo@mercadolibre.com.co>
2023-05-09 15:23:56 -0500

    :sparkles: multiple components

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+function Navbar({tasksCount}) {
+    return <div className="navbar bg-base-100">
+        <div className="flex-1">
+            <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+        </div>
+        <div className="flex-none">
+            <ul className="menu menu-horizontal px-1">
+                <li><a>Tareas actuales: {tasksCount}</a></li>
+            </ul>
+        </div>
+    </div>;
+}
+
+export default Navbar
---
+++ src/pages/count-task.jsx
@@ -0,0 +1,5 @@
+function CountTask() {
+
+}
+
+export default CountTask
